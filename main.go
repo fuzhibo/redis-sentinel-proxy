070b4ce96ae4e915f3b4777102dd85f4c4436da7
ZhiBo Fu <fuzhibo@tom.com>
2024-12-20 15:03:43 +0800

    Optimized code and error handing.

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 
 var (
 	masterAddrChan chan *net.TCPAddr = make(chan *net.TCPAddr, 10)
-	raddr          *net.TCPAddr
-	saddrs         []*net.TCPAddr
+	// raddr          *net.TCPAddr
+	saddrs []*net.TCPAddr
 
 	localAddr    = flag.String("listen", ":9999", "local address")
 	sentinelAddr = flag.String("sentinel", ":26379", "remote address, split with ','")
@@ -26,7 +26,7 @@ func main() {
 
 	laddr, err := net.ResolveTCPAddr("tcp", *localAddr)
 	if err != nil {
-		log.Fatal("Failed to resolve local address: ", err)
+		log.Fatal("Failed to resolve local address: ", *localAddr, " : ", err)
 	}
 	// check if "," in the sentinelAddr string
 	if strings.Contains(*sentinelAddr, ",") {
@@ -36,7 +36,6 @@ func main() {
 			saddr, err := net.ResolveTCPAddr("tcp", addr)
 			if err != nil {
 				log.Fatal("Failed to resolve sentinel address ", addr, " : ", err)
-				continue
 			}
 			saddrs = append(saddrs, saddr)
 		}
@@ -75,7 +74,7 @@ func master() {
 	var err_count = 0
 	for {
 		if err_count > 100 {
-			log.Fatal("Failed get master addr from ", saddrs[inx%len(saddrs)], " will not retry...")
+			log.Fatal("Failed get master addr from ", saddrs[inx%len(saddrs)], " attach max limit ", err_count, " will not retry...")
 		}
 		masterAddr, err := getMasterAddr(saddrs[inx%len(saddrs)], *masterName)
 		if err != nil {
@@ -96,12 +95,23 @@ func master() {
 	}
 }
 
-func pipe(r io.Reader, w io.WriteCloser) {
-	io.Copy(w, r)
-	w.Close()
+func pipe(r net.Conn, w net.Conn) {
+	defer r.Close()
+	defer w.Close()
+	n, err := io.Copy(w, r)
+	if err != nil {
+		log.Println("failed to pipe data, copy ", n, " bytes from ", w.RemoteAddr().String(), " -> ", r.RemoteAddr().String(), " due to ", err)
+		return
+	}
+	if n == 0 {
+		log.Printf("failed to forward %s -> %s due to %v", w.RemoteAddr().String(), r.RemoteAddr().String(), err)
+		return
+	} else {
+		log.Printf("forward %d bytes for %s -> %s", n, w.RemoteAddr().String(), r.RemoteAddr().String())
+	}
 }
 
-func proxy(local io.ReadWriteCloser) {
+func proxy(local net.Conn) {
 	remoteAddr := <-masterAddrChan
 	remote, err := net.DialTCP("tcp", nil, remoteAddr)
 	if err != nil {
@@ -112,6 +122,8 @@ func proxy(local io.ReadWriteCloser) {
 	go pipe(local, remote)
 	go pipe(remote, local)
 }
+
+const ErrMasterAddressNotFound = "couldn't get master address from sentinel"
 
 func getMasterAddr(sentinelAddress *net.TCPAddr, masterName string) (*net.TCPAddr, error) {
 	conn, err := net.DialTCP("tcp", nil, sentinelAddress)
@@ -132,7 +144,7 @@ func getMasterAddr(sentinelAddress *net.TCPAddr, masterName string) (*net.TCPAdd
 	parts := strings.Split(string(b), "\r\n")
 
 	if len(parts) < 5 {
-		err = errors.New("Couldn't get master address from sentinel")
+		err = errors.New(ErrMasterAddressNotFound)
 		return nil, err
 	}
 
